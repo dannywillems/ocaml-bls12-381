a0a641c4bcfd07743c9e51d5ed7a61f9144168c0
Danny Willems <be.danny.willems@gmail.com>
2021-05-25 16:27:01 +0200

    Remove commented conversion routines

--- src/unix/g1.ml
+++ src/unix/g1.ml
@@ -93,17 +93,4 @@ module Stubs = struct
     if res = false then None else Some buffer
 end
 
-(* let compressed_of_uncompressed g =
- *   let buffer = Compressed.empty () in
- *   G1_stubs.compressed_of_uncompressed
- *     (Ctypes.ocaml_bytes_start (Compressed.to_bytes buffer))
- *     (Ctypes.ocaml_bytes_start (Uncompressed.to_bytes g)) ;
- *   Compressed.of_bytes_exn (Compressed.to_bytes buffer)
- * 
- * let uncompressed_of_compressed g =
- *   let buffer = Uncompressed.empty () in
- *   G1_stubs.uncompressed_of_compressed
- *     (Ctypes.ocaml_bytes_start (Uncompressed.to_bytes buffer))
- *     (Ctypes.ocaml_bytes_start (Compressed.to_bytes g)) ;
- *   Uncompressed.of_bytes_exn (Uncompressed.to_bytes buffer) *)
 include Bls12_381_gen.G1.Make (Fr) (Stubs)
