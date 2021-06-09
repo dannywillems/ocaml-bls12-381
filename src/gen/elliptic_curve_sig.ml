fca83170d6cba16b78c7be686b89e7d23e19404e
Danny Willems <be.danny.willems@gmail.com>
2021-06-09 15:24:06 +0200

    Use first class modules for FFT and iFFT

--- src/gen/elliptic_curve_sig.ml
+++ src/gen/elliptic_curve_sig.ml
@@ -246,55 +246,39 @@ struct
     assert (Bytes.length res = size_in_bytes) ;
     res
 
-  let bitreverse n l =
-    let r = ref 0 in
-    let n = ref n in
-    for _i = 0 to l - 1 do
-      r := (!r lsl 1) lor (!n land 1) ;
-      n := !n lsr 1
-    done ;
-    !r
-
   let fft ~domain ~points =
-    (* See
-       https://gitlab.com/dannywillems/ocaml-polynomial/-/blob/8351c266c4eae185823ab87d74ecb34c0ce70afe/src/polynomial.ml#L428
-    *)
-    let reorg_coefficients n logn coefficients =
-      for i = 0 to n - 1 do
-        let reverse_i = bitreverse i logn in
-        if i < reverse_i then (
-          let a_i = coefficients.(i) in
-          let a_ri = coefficients.(reverse_i) in
-          coefficients.(i) <- a_ri ;
-          coefficients.(reverse_i) <- a_i )
-      done
-    in
-    let n = Array.length domain in
-    let logn = Z.log2 (Z.of_int n) in
-    let output = Array.of_list points in
-    reorg_coefficients n logn output ;
-    let m = ref 1 in
-    for _i = 0 to logn - 1 do
-      let exponent = n / (2 * !m) in
-      let k = ref 0 in
-      while !k < n do
-        for j = 0 to !m - 1 do
-          let w = domain.(exponent * j) in
-          (* odd *)
-          let right = mul output.(!k + j + !m) w in
-          output.(!k + j + !m) <- add output.(!k + j) (negate right) ;
-          output.(!k + j) <- add output.(!k + j) right
-        done ;
-        k := !k + (!m * 2)
-      done ;
-      m := !m * 2
-    done ;
-    Array.to_list output
+    let module M = struct
+      type group = t
+
+      type scalar = Scalar.t
+
+      let mul = mul
+
+      let add = add
+
+      let sub x y = add x (negate y)
+
+      let inverse_exn_scalar = Scalar.inverse_exn
+
+      let scalar_of_z = Scalar.of_z
+    end in
+    Fft.fft (module M) ~domain ~points
 
   let ifft ~domain ~points =
-    let power = Array.length domain in
-    assert (power = List.length points) ;
-    let points = fft ~domain ~points in
-    let power_inv = Scalar.inverse_exn (Scalar.of_z (Z.of_int power)) in
-    List.map (fun g -> mul g power_inv) points
+    let module M = struct
+      type group = t
+
+      type scalar = Scalar.t
+
+      let mul = mul
+
+      let add = add
+
+      let sub x y = add x (negate y)
+
+      let inverse_exn_scalar = Scalar.inverse_exn
+
+      let scalar_of_z = Scalar.of_z
+    end in
+    Fft.ifft (module M) ~domain ~points
 end
