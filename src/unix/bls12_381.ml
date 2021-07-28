bac72ee6cb1c606dc8b58c870006b29d0acd6682
Danny Willems <be.danny.willems@gmail.com>
2021-07-28 18:37:05 +0200

    Rust backend: mock implementation of Signature

--- src/unix/bls12_381.ml
+++ src/unix/bls12_381.ml
@@ -27,3 +27,62 @@ module Fq12 = Fq12
 module G1 = G1
 module G2 = G2
 module Pairing = Pairing
+
+module Signature = struct
+  type sk = Bytes.t
+
+  type pk = Bytes.t
+
+  type signature = Bytes.t
+
+  let sk_of_bytes_exn _sk = failwith "Not implemented"
+
+  let sk_to_bytes _b = failwith "Not implemented"
+
+  let unsafe_pk_of_bytes _ = failwith "Not implemented"
+
+  let pk_of_bytes_exn _ = failwith "Not implemented"
+
+  let pk_of_bytes_opt _ = failwith "Not implemented"
+
+  let pk_to_bytes _ = failwith "Not implemented"
+
+  let generate_sk ?key_info ikm =
+    ignore key_info ;
+    ignore ikm ;
+    failwith "Not implemented"
+
+  let derive_pk _sk = failwith "Not implemented"
+
+  let aggregate_signature_opt _ = failwith "Not implemented"
+
+  module Basic = struct
+    let sign _sk _message = failwith "Not implemented"
+
+    let verify _pk _msg _signature = failwith "Not implemented"
+
+    let aggregate_verify _ _ = failwith "Not implemented"
+  end
+
+  module Aug = struct
+    let sign _sk _message = failwith "Not implemented"
+
+    let verify _pk _msg _signature = failwith "Not implemented"
+
+    let aggregate_verify _ _ = failwith "Not implemented"
+  end
+
+  module Pop = struct
+    type proof = Bytes.t
+
+    let sign _sk _message = failwith "Not implemented"
+
+    let verify _pk _msg _signature = failwith "Not implemented"
+
+    let pop_prove _sk = failwith "Not implemented"
+
+    let pop_verify _pk _signature = failwith "Not implemented"
+
+    let aggregate_verify _ _ _ = failwith "Not implemented"
+  end
+end
