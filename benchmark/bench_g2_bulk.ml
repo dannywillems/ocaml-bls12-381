c1c70380a28af2322e44927eb05549f0e8363231
Danny Willems <be.danny.willems@gmail.com>
2021-09-30 18:46:10 +0200

    Bench sum of scalar multiplications

--- benchmark/bench_g2_bulk.ml
+++ benchmark/bench_g2_bulk.ml
@@ -14,7 +14,7 @@ let () =
   in
   let ratio = res_bulk /. res_sequential in
   Printf.printf
-    "Add %d elements of G2: bulk %f ms and %f ms (sequential)\n\
+    "Add %d elements of G2: bulk %f ms and sequential %f ms \n\
      It is a gain of %f pcts"
     n
     res_bulk
@@ -24,21 +24,51 @@ let () =
 let () =
   let open Bls12_381 in
   let n = 100_000 in
-  let xs = List.init n (fun _ -> (G2.random (), G2.Scalar.random ())) in
-  let bulk_start_time = Sys.time () in
-  ignore @@ G2.add_mul_bulk xs ;
-  let bulk_end_time = Sys.time () in
+  let gs = List.init n (fun _ -> G2.random ()) in
+  let ss = List.init n (fun _ -> G2.Scalar.random ()) in
   let sequential_start_time = Sys.time () in
   ignore
-  @@ List.fold_left (fun acc (g, n) -> G2.add acc (G2.mul g n)) G2.zero xs ;
+  @@ List.fold_left2 (fun acc g n -> G2.add acc (G2.mul g n)) G2.zero gs ss ;
   let sequential_end_time = Sys.time () in
+  let bulk_start_time = Sys.time () in
+  List.iter2 (fun g n -> G2.mul_inplace g n) gs ss ;
+  ignore @@ G2.add_bulk gs ;
+  let bulk_end_time = Sys.time () in
   let res_bulk = (bulk_end_time -. bulk_start_time) *. 1000. in
   let res_sequential =
     (sequential_end_time -. sequential_start_time) *. 1000.
   in
   let ratio = res_bulk /. res_sequential in
   Printf.printf
-    "Add and mul of %d elements of G2: bulk %f ms and %f ms (sequential)\n\
+    "Sum of scalar multiplications of %d elements of G2: bulk %f ms and \
+     sequential %f ms \n\
+     It is a gain of %f pcts"
+    n
+    res_bulk
+    res_sequential
+    (1. -. ratio)
+
+let () =
+  let open Bls12_381 in
+  let n = 100_000 in
+  let gs = List.init n (fun _ -> G2.random ()) in
+  let ss = List.init n (fun _ -> G2.Scalar.random ()) in
+  let sequential_start_time = Sys.time () in
+  ignore
+  @@ List.fold_left2 (fun acc g n -> G2.add acc (G2.mul g n)) G2.zero gs ss ;
+  let sequential_end_time = Sys.time () in
+  let bulk_start_time = Sys.time () in
+  List.iter2 (fun g n -> G2.mul_inplace g n) gs ss ;
+  ignore @@ G2.add_bulk gs ;
+  let bulk_end_time = Sys.time () in
+  let res_bulk = (bulk_end_time -. bulk_start_time) *. 1000. in
+  let res_sequential =
+    (sequential_end_time -. sequential_start_time) *. 1000.
+  in
+  let ratio = res_bulk /. res_sequential in
+  Printf.printf
+    "Sum of scalar multiplication of %d elements of G2: with add_bulk and \
+     mul_inplace %f ms and %f ms (sequential)\n\
      It is a gain of %f pcts"
     n
     res_bulk
