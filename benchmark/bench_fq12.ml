9dcea1fcb1164bd2d56f367b70fed14a42f5770d
ambrona <mac.ambrona@gmail.com>
2022-02-01 12:29:48 +0100

    Add benchmark on Fp12 multiplication (Fp12.pow)

--- benchmark/bench_fq12.ml
+++ benchmark/bench_fq12.ml
@@ -4,10 +4,16 @@ module MakeBenchBlst = struct
   let test_addition =
     let e1 = Bls12_381.Fq12.random () in
     let e2 = Bls12_381.Fq12.random () in
-    Bench.Test.create ~name:"Addition Fq12" (fun () ->
+    Bench.Test.create ~name:"Addition on Fq12" (fun () ->
         ignore (Bls12_381.Fq12.mul e1 e2))
 
-  let get_benches () = [test_addition]
+  let test_multiplication =
+    let p = Bls12_381.Fq12.random () in
+    let exp = Bls12_381.Fr.random () |> Bls12_381.Fr.to_z in
+    Bench.Test.create ~name:"Multiplication on Fq12" (fun () ->
+        ignore (Bls12_381.Fq12.pow p exp))
+
+  let get_benches () = [test_addition; test_multiplication]
 end
 
 let () =
