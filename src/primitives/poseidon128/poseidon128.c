e469ce79f77ce12293d8ad5b05600a31c50aad89
Danny Willems <be.danny.willems@gmail.com>
2022-02-18 21:14:24 +0100

    Implement linear optimisation for HADES

--- src/primitives/poseidon128/poseidon128.c
+++ src/primitives/poseidon128/poseidon128.c
@@ -9,6 +9,7 @@ size_t poseidon128_ctxt_sizeof() { return sizeof(poseidon128_ctxt_t); }
 
 int poseidon128_constants_init(blst_fr *ark, blst_fr **mds, int ark_len,
                                int mds_nb_rows, int mds_nb_cols) {
+
   if (NB_CONSTANTS != ark_len || mds_nb_rows != WIDTH || mds_nb_cols != WIDTH) {
     return 1;
   }
@@ -62,29 +63,82 @@ void apply_matrix_multiplication(poseidon128_ctxt_t *ctxt) {
   }
 }
 
+blst_fr *get_next_round_key(poseidon128_ctxt_t *ctxt) {
+  return (POSEIDON128_ARK + ctxt->i_round_key++);
+}
+
 void apply_cst(poseidon128_ctxt_t *ctxt) {
   for (int i = 0; i < WIDTH; i++) {
-    blst_fr_add(&ctxt->s[i], &ctxt->s[i], POSEIDON128_ARK + ctxt->i_round_key);
-    ctxt->i_round_key++;
+    blst_fr_add(&ctxt->s[i], &ctxt->s[i], get_next_round_key(ctxt));
+  }
+}
+
+void apply_batched_partial_round(poseidon128_ctxt_t *ctxt) {
+
+  blst_fr buffer;
+  blst_fr intermediary_state[WIDTH + NB_TMP_VAR];
+  for (int i = 0; i < WIDTH; i++) {
+    memcpy(intermediary_state + i, &ctxt->s[i], sizeof(blst_fr));
+  }
+
+  // Apply sbox on the last element of the state
+  blst_fr_sqr(&buffer, intermediary_state + WIDTH - 1);
+  blst_fr_sqr(&buffer, &buffer);
+  blst_fr_mul(intermediary_state + WIDTH - 1, &buffer,
+              intermediary_state + WIDTH - 1);
+
+  // Computing the temporary variables
+  for (int i = 0; i < NB_TMP_VAR; i++) {
+    // we start with the first element
+    blst_fr_mul(intermediary_state + WIDTH + i, get_next_round_key(ctxt),
+                intermediary_state);
+    for (int j = 1; j < WIDTH + i; j++) {
+      blst_fr_mul(&buffer, get_next_round_key(ctxt), intermediary_state + j);
+      blst_fr_add(intermediary_state + WIDTH + i,
+                  intermediary_state + WIDTH + i, &buffer);
+    }
+    // We add the constant
+    blst_fr_add(intermediary_state + WIDTH + i, intermediary_state + WIDTH + i,
+                get_next_round_key(ctxt));
+
+    // Applying sbox
+    blst_fr_sqr(&buffer, intermediary_state + i + WIDTH);
+    blst_fr_sqr(&buffer, &buffer);
+    blst_fr_mul(intermediary_state + i + WIDTH, &buffer,
+                intermediary_state + i + WIDTH);
+  }
+
+  // Computing the final state
+  for (int i = 0; i < WIDTH; i++) {
+    blst_fr_mul(&ctxt->s[i], get_next_round_key(ctxt), intermediary_state);
+    for (int j = 1; j < WIDTH + NB_TMP_VAR; j++) {
+      blst_fr_mul(&buffer, intermediary_state + j, get_next_round_key(ctxt));
+      blst_fr_add(&ctxt->s[i], &buffer, &ctxt->s[i]);
+    }
+    blst_fr_add(&ctxt->s[i], &ctxt->s[i], get_next_round_key(ctxt));
   }
 }
 
 void poseidon128_apply_perm(poseidon128_ctxt_t *ctxt) {
   ctxt->i_round_key = 0;
+  apply_cst(ctxt);
   for (int i = 0; i < NB_FULL_ROUNDS / 2; i++) {
-    apply_cst(ctxt);
     apply_sbox(ctxt, 1);
     apply_matrix_multiplication(ctxt);
-  }
-  for (int i = 0; i < NB_PARTIAL_ROUNDS; i++) {
     apply_cst(ctxt);
+  }
+  for (int i = 0; i < NB_BATCHED_PARTIAL_ROUNDS; i++) {
+    apply_batched_partial_round(ctxt);
+  }
+  for (int i = 0; i < NB_UNBATCHED_PARTIAL_ROUNDS; i++) {
     apply_sbox(ctxt, 0);
     apply_matrix_multiplication(ctxt);
+    apply_cst(ctxt);
   }
   for (int i = 0; i < NB_FULL_ROUNDS / 2; i++) {
-    apply_cst(ctxt);
     apply_sbox(ctxt, 1);
     apply_matrix_multiplication(ctxt);
+    apply_cst(ctxt);
   }
 }
 
