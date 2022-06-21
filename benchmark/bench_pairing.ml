b6cf80361b0d22e1b49d09c923c792b5d1947fa6
Danny Willems <be.danny.willems@gmail.com>
2022-06-21 14:03:52 +0200

    Format benchmkar miller loop

--- benchmark/bench_pairing.ml
+++ benchmark/bench_pairing.ml
@@ -48,10 +48,10 @@ let t3 =
 let miller_loop_benches =
   let open Bls12_381 in
   List.init 10 ~f:(fun i ->
-    let p = List.init (1 + i) ~f:(fun _i -> (G1.random (), G2.random ())) in
-    let name = Printf.sprintf "Miller loop on %d couples of points" (i + 1) in
-    Bench.Test.create ~name (fun () ->
-        ignore @@ Bls12_381.Pairing.miller_loop p))
+      let p = List.init (1 + i) ~f:(fun _i -> (G1.random (), G2.random ())) in
+      let name = Printf.sprintf "Miller loop on %d couples of points" (i + 1) in
+      Bench.Test.create ~name (fun () ->
+          ignore @@ Bls12_381.Pairing.miller_loop p))
 
 let t5 =
   let open Bls12_381 in
@@ -62,4 +62,5 @@ let t5 =
     (fun () ->
       ignore @@ Bls12_381.Pairing.(final_exponentiation_exn (miller_loop p)))
 
-let () = Core.Command.run (Bench.make_command ([t1; t2; t3; t5] @ miller_loop_benches))
+let () =
+  Core.Command.run (Bench.make_command ([t1; t2; t3; t5] @ miller_loop_benches))
